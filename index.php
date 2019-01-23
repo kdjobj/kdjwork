055f5a2e56016449f142621ac9042d684fe8fa43
dongju kwak <kdjobj@gmail.com>
2019-01-23 13:31:24 +0900

    add all

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width">
+	<link type="text/css" href="/css/style.css" rel="stylesheet" />
+	<script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+</head>
+<body id="home">
+
+
+<!-- trendset-coming-soon-web-template CSS -->
+<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=PT+Sans+Narrow:regular,bold">
+<link rel="stylesheet" type="text/css" href="/plugin/trendset-coming-soon-web-template/css/styles.css">
+
+
+
+<div id="Header">
+	<div class="wrapper">
+		<img src="/img/mainheader.png"/>
+	</div>
+</div>
+<div id="Content" class="wrapper">
+	<div class="countdown styled"></div>
+	<div style="text-align:center;">
+		<img src="/img/mainfooter.png" id="footer"/>
+	</div>
+</div>
+	
+
+<div id="overlay"></div>
+
+<!-- trendset-coming-soon-web-template Scripts -->
+<script type="text/javascript" src="/plugin/trendset-coming-soon-web-template/js/Backstretch.js"></script>
+<script type="text/javascript" src="/plugin/trendset-coming-soon-web-template/js/jquery.countdown.js"></script>
+<script type="text/javascript" src="/plugin/trendset-coming-soon-web-template/js/global.js"></script>
+
+
+
+
+</body>
+</html>
